70fd61aae9f3d590f38ccf5d3da2cb82d89489df
Ashraf763 <imashraf07@gmai.com>
2025-07-18 18:51:42 +0530

    added / route in server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
